5ae600656b69a3ed66ca2af9a0677c3b8816c86f
jiang yong <ugyn@qq.com>
2013-12-02 10:19:53 +0800

    Helloworld 实例第二步

---
+++ src/cn/stdin/struts/helloworld/model/MessageStore.java
@@ -0,0 +1,21 @@
+package cn.stdin.struts.helloworld.model;
+
+public class MessageStore {
+	 private String message;
+     
+	    public MessageStore() {
+	         
+	        setMessage("Hello Struts User");
+	    }
+	 
+	    public String getMessage() {
+	 
+	        return message;
+	    }
+	 
+	    public void setMessage(String message) {
+	 
+	        this.message = message;
+	    }
+
+}
